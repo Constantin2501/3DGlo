ea4c3c4113da3c0af678322a9f0a6250b80bfbdc
Konstantin Krotov <ckwebmast@gmail.com>
2023-06-12 15:33:50 +0300

    complete lesson20

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,17 +2,26 @@ const modal = () => {
     const modal = document.querySelector('.popup')
     const buttons = document.querySelectorAll('.popup-btn')
     const closeBtn = modal.querySelector('.popup-close')
-    console.log(closeBtn);
 
     buttons.forEach(btn => {
        btn.addEventListener('click', () => {
         modal.style.display = 'block'
+        if (screen.width > 768) {
+            modal.animate([
+                {opacity: 0},
+                {opacity: 1},
+            ], 1000)
+        }
+        
        })
     })
 
     closeBtn.addEventListener('click', () => {
         modal.style.display = 'none'
     })
+
+
+
 }
 
 export default modal
