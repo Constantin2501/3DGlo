4583b830742d77c0a7061cc5976b8d8f6f8cc83a
Konstantin Krotov <ckwebmast@gmail.com>
2023-06-13 16:24:15 +0300

    start lesson22

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import validation from "./modules/validation";
+import tabs from "./modules/tabs";
 
 
 timer('19 june 2023')
 menu()
 modal()
 validation()
+tabs()
