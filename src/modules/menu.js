fab53f157fc1f459dbc9758879b6c7b6817a50cc
Konstantin Krotov <ckwebmast@gmail.com>
2023-06-13 19:00:27 +0300

    refact menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -5,7 +5,6 @@ const menu = () => {
 
 
     body.addEventListener('click', (e) => {
-        console.log(e.target);
         if (e.target.closest('.menu') || !e.target.classList.contains('active-menu')) {
             menu.classList.toggle('active-menu')
         }
