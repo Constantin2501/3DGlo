d77d75a62d6a683384528ec04e916d4979d8f6fb
Konstantin Krotov <ckwebmast@gmail.com>
2023-06-14 20:43:42 +0300

    add calc

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from "./modules/modal";
 import validation from "./modules/validation";
 import tabs from "./modules/tabs";
 import slider from "./modules/slider";
+import calc from "./modules/calc";
 
 
 timer('19 june 2023')
@@ -12,3 +13,4 @@ modal()
 validation()
 tabs()
 slider()
+calc(100)
